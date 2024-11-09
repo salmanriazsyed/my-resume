a0812dacd32b539f349a68aac9c7d40d4c9c2ebe
Salman Riaz Syed <salmanriazsyeduk@gmail.com>
2024-11-09 22:00:11 +0500

    Terminal window minimum height set to 650 pixels.

--- script.js
+++ script.js
@@ -24,19 +24,19 @@ function typeText(text, index = 0) {
     setTimeout(() => typeText(text, index + 1), 1); // Adjust speed of typing here
   } else {
     // If text is finished, increase height of terminal window based on content
-    // adjustTerminalHeight();
+     //adjustTerminalHeight();
   }
 }
-
+/*
 // Function to adjust the terminal window height dynamically
 function adjustTerminalHeight() {
   const terminal = document.querySelector('.terminal');
   const lineHeight = parseInt(window.getComputedStyle(terminal).lineHeight);
   const lines = terminal.innerText.split('\n').length;
-  // const newHeight = Math.min(lines * lineHeight + 300, 650); // Set max height to 800px
-  // terminal.style.height = `${newHeight}px`;
+  const newHeight = Math.min(lines * lineHeight + 300, 650); // Set max height to 800px
+  terminal.style.height = `${newHeight}px`;
 }
-
+*/
 // Start typing the resume when the page loads
 window.onload = () => {
   typeText(resumeText);
